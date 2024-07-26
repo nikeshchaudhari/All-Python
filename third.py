ece91fec6f6658f74d73d868f92c1ae1ba01681d
Nikesh Chaudhari <info.nikesh12@g,ail.com>
2024-07-26 22:40:40 +0545

    Everyday

---
+++ third.py
@@ -0,0 +1,35 @@
+# lists & Turples
+
+# Array || Lists
+
+# marks = [98,25,56,52,48]
+
+# print(len(marks))
+# String are immutable
+
+student = ["raju",85,"Kathmandu"]
+# print(student)
+
+student[0]= "Rajan"
+# print(student)
+
+
+# str = "hello"
+# print(str[0])
+# str[0] ="y"   not change
+
+# List Slicing
+
+# marks = [10,20,30,40,50]
+# print(marks[-5:-3])
+
+
+# List Method
+
+list = ["banan","mango","apple","litchi"]
+# list.append(47)
+# list.sort()
+# list.sort(reverse = True)
+# list.reverse()
+list.insert(1,"Cat")
+print(list)
