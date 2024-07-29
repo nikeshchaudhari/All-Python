7b95a3581556359e452fd5be047576418f55cbb6
Nikesh Chaudhari <info.nikesh12@g,ail.com>
2024-07-29 22:58:39 +0545

    Tuple Learn

---
+++ tuple.py
@@ -0,0 +1,38 @@
+tup = (1,2,3,4,2,3)
+
+# tup[0] = 43 Not allowed
+
+# print(type(tup))
+
+# Tuple Slicing
+
+# print(tup.index(1))
+# print(tup.count(2))
+# movies = []
+# mov1 = input("Enter 1 Movies   :")
+# mov2 = input("Enter 2 Movies : ")
+# mov3 = input("Enter 3 Movies :")
+
+# Movie = [mov1,mov2,mov3]
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+
+
+# print(movies)
+
+# list = [1,2,1]
+# list2 = [1,2,3]
+
+# ls = list.copy()
+# list.reverse()
+
+# if(ls == list):
+#     print("palidrom")
+# else:
+#     print("not Palindrom")
+
+std = ["C","D","B","A"]
+print(std.count("A"))
+std.sort()
+print(std)
