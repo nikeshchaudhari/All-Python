aed9e695140d43f3cf5a422ff90cb369908c9fb3
Nikesh Chaudhari <info.nikesh12@g,ail.com>
2024-07-30 21:33:46 +0545

    Everyday

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+# Dictionary 
+# no order in dictionary || No create dublicate key
+info = {
+    "name"  : "Nikesh",
+    "subject" :["c","c++","java"],
+    "cgpa ":2.6,
+    "marks" : [50,80,90]
+}
+
+null = {}
+print(null)
+print(info["name"])  
+
+info["name"] = "Python" #override
+info["surname"] = "B"
+
+print(info)
+
+
---
+++ nested_dictionaries.py
@@ -0,0 +1,14 @@
+student = {
+    "name" :"Kumar",
+    "subject" :{
+        "phy" : 65,
+        "math" : 56.0,
+    }
+}
+# print(student["subject"]["phy"])
+
+
+# Method
+# .keys
+print(list(student.keys()))
+print(len(student))
