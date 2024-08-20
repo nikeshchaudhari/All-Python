6d50f1dc24bd372c9f37aa142cb9ba8ef7ad815b
Nikesh Chaudhari <info.nikesh12@gmail.com>
2024-08-20 23:03:44 +0545

    Py loop

---
+++ whileLoop.py
@@ -0,0 +1,56 @@
+# Loops are used to repeat instructions.
+
+# For Loop
+# While  Loop 
+
+
+
+# i = 1
+# while  i<=500 :
+#     print("Hello", i)
+#     i+=1
+
+# print(i) 
+'''
+
+i = 10
+while i>=1 :
+    print("Hello World", i)
+    i-=1
+
+
+print("loop Ended")
+
+'''
+
+# print number from 1 to 100
+
+# i =1
+# while i<=100:
+#     print(i)
+#     i+=1
+
+
+# i=90
+# while i>=1 :
+#     print(i)
+#     i-=9
+'''
+ Print Multipliation
+n = int(input("Enter Number : "))
+i = 1
+
+while i<=10:
+    print(i*n)
+    i+=1
+
+    '''
+
+# qs4
+
+nums =[1,4,9,16,25,36,49,64,81,100]
+
+index = 0
+while index < len(nums):
+    print(nums[index])
+    index +=1
